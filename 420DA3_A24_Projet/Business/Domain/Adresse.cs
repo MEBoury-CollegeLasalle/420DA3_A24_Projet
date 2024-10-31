a6f19b5444b1852619d640e851f8e126ff1c4bb3
maguette03 <maguette_laye.thiaw_tall@lcieducation.net>
2024-10-30 20:03:49 -0400

    Creation de la class Adresse

---
+++ 420DA3_A24_Projet/Business/Domain/Adresse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420DA3_A24_Projet.Business.Domain
+{
+    public class Adresse
+    {
+       public int Id { get; set; }
+    }
+}
