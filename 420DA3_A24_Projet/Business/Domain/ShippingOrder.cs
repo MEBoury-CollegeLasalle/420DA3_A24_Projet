34942e8590774b0e19c19cf6cde8968451832552
Cindy Kengmogne <kengmognecindyb@gmail.com>
2024-10-31 23:41:09 -0400

    Creation de la classe ShippingOrder et WareHouse, et de leurs attributs prives

---
+++ 420DA3_A24_Projet/Business/Domain/ShippingOrder.cs
@@ -0,0 +1,29 @@
+﻿using Project_Utilities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420DA3_A24_Projet.Business.Domain
+{
+    public class ShippingOrder
+    {
+        private int id;
+        private ShippingOrderStatusEnum status;
+        private int sourceClientId;
+        private int creatorEmployeeId;
+        private int destinationAddressId;
+        private int? fulfillerEmployeeId;
+        private DateTime? shippingDate;
+        private DateTime dateCreated;
+        private DateTime? dateModified;
+        private DateTime? dateDeleted;
+        // private Client sourceClient;
+        private Shipment? shipment;
+        private User creatorEmployee;
+        private User? fulfillerEmployee;
+        private Adresse destinationAddress;
+        // private List<ShippingOrderProduct> ShippingOrderProducts;
+    }
+}
---
+++ 420DA3_A24_Projet/Business/Domain/Warehouse.cs
@@ -0,0 +1,26 @@
+﻿using Project_Utilities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420DA3_A24_Projet.Business.Domain
+{
+    public class Warehouse
+    {
+        public const int WAREHOUSE_NAME_MAX_LENGTH = 128;
+        public const int WAREHOUSE_NAME_MIN_LENGTH = 4;
+
+        private int id;
+        private string warehouseName;
+        private int addressId;
+        private DateTime dateCreated;
+        private DateTime? dateModified;
+        private DateTime? dateDeleted;
+        //private List<Client> clients;
+        private Adresse address;
+        //private List<PurchaseOrder> RestockOrders;
+        private List<User> warehouseEmployees;
+    }
+}
