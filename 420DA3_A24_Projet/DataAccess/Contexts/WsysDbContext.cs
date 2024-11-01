09fbe7cd5115d0d4c4070d35e052c5dc63d53dd1
Marc-Eric Boury <marc-eric.boury@collegelasalle.com>
2024-11-01 14:52:58 -0400

    ajout configuration entité User

--- 420DA3_A24_Projet/DataAccess/Contexts/WsysDbContext.cs
+++ 420DA3_A24_Projet/DataAccess/Contexts/WsysDbContext.cs
@@ -15,7 +15,7 @@ internal class WsysDbContext : DbContext {
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
         base.OnConfiguring(optionsBuilder);
 
-        optionsBuilder
+        _ = optionsBuilder
             .UseSqlServer("") // TODO: Add connection string
             .UseLazyLoadingProxies();
     }
@@ -23,6 +23,107 @@ internal class WsysDbContext : DbContext {
     protected override void OnModelCreating(ModelBuilder modelBuilder) {
         base.OnModelCreating(modelBuilder);
 
+
+        #region USER
+
+        _ = modelBuilder.Entity<User>()
+            .ToTable(nameof(this.Users))
+            .HasKey(user => user.Id);
+
+        _ = modelBuilder.Entity<User>()
+            .HasIndex(user => user.Username)
+            .IsUnique(true);
+
+        _ = modelBuilder.Entity<User>()
+            .Property(user => user.Id)
+            .HasColumnName("Id")
+            .HasColumnOrder(0)
+            .HasColumnType("int")
+            .UseIdentityColumn(1, 1);
+
+        _ = modelBuilder.Entity<User>()
+            .Property(user => user.Username)
+            .HasColumnName("Username")
+            .HasColumnOrder(1)
+            .HasColumnType($"nvarchar({User.USERNAME_MAX_LENGTH})")
+            .HasMaxLength(User.USERNAME_MAX_LENGTH)
+            .IsRequired(true);
+
+        _ = modelBuilder.Entity<User>()
+            .Property(user => user.PasswordHash)
+            .HasColumnName("PasswordHash")
+            .HasColumnOrder(2)
+            .HasColumnType($"nvarchar({User.PASSWORDHASH_MAX_LENGTH})")
+            .HasMaxLength(User.PASSWORDHASH_MAX_LENGTH)
+            .IsRequired(true);
+
+        _ = modelBuilder.Entity<User>()
+            .Property(user => user.EmployeeWarehouseId)
+            .HasColumnName("EmployeeWarehouseId")
+            .HasColumnOrder(3)
+            .HasColumnType("int")
+            .IsRequired(false);
+
+        _ = modelBuilder.Entity<User>()
+            .Property(user => user.DateCreated)
+            .HasColumnName("DateCreated")
+            .HasColumnOrder(4)
+            .HasColumnType("datetime2")
+            .HasPrecision(7)
+            .HasDefaultValueSql("GETDATE()")
+            .IsRequired(true);
+
+        _ = modelBuilder.Entity<User>()
+            .Property(user => user.DateModified)
+            .HasColumnName("DateModified")
+            .HasColumnOrder(5)
+            .HasColumnType("datetime2")
+            .HasPrecision(7)
+            .IsRequired(false);
+
+        _ = modelBuilder.Entity<User>()
+            .Property(user => user.DateDeleted)
+            .HasColumnName("DateDeleted")
+            .HasColumnOrder(6)
+            .HasColumnType("datetime2")
+            .HasPrecision(7)
+            .IsRequired(false);
+
+        _ = modelBuilder.Entity<User>()
+            .Property(user => user.RowVersion)
+            .HasColumnName("RowVersion")
+            .HasColumnOrder(7)
+            .IsRowVersion();
+
+        _ = modelBuilder.Entity<User>()
+            .HasMany(user => user.Roles)
+            .WithMany(role => role.Users);
+
+        #endregion
+
+        #region ROLE
+
+        // TODO: @PROF Faire config Role
+
+        #endregion
+
+        #region SHIPPING_ORDER
+
+        // TODO: @PROF Faire config ShippingOrder
+
+        #endregion
+
+        #region PURCHASE_ORDER
+
+        // TODO: @PROF Faire config PurchaseOrder
+
+        #endregion
+
+        #region PIVOT - SHIPPING_ORDER_PRODUCTS
+
+        // TODO: @PROF Faire config table pivot. NOTE: relation N-à-N complexe.
+
+        #endregion
 
         // TODO: @TOUTE_EQUIPE Faites la configuration de vos entités et de leur relations ici
 
