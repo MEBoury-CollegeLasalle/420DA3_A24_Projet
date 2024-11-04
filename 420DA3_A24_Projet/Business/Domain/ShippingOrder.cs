38430ce94ad71d0114297806c490e56fd1162dee
Cindy Kengmogne <kengmognecindyb@gmail.com>
2024-11-04 13:02:49 -0500

    Changement des attributs prives en attributs publiques dans les classes warehouse et ShippingOrder, Configuration des attributs de la classe Warehouse.

--- 420DA3_A24_Projet/Business/Domain/ShippingOrder.cs
+++ 420DA3_A24_Projet/Business/Domain/ShippingOrder.cs
@@ -11,22 +11,22 @@ namespace _420DA3_A24_Projet.Business.Domain
     {
         // TODO: @CINDY Attention: avec EF, il faut utiliser des propriétés publiques.
         // Là vous utilisez des champs privés. Faites attntion aussi à la casse (PascalCase pour propriétés publiques)
-        private int id;
-        private ShippingOrderStatusEnum status;
-        private int sourceClientId;
-        private int creatorEmployeeId;
-        private int destinationAddressId;
-        private int? fulfillerEmployeeId;
-        private DateTime? shippingDate;
-        private DateTime dateCreated;
-        private DateTime? dateModified;
-        private DateTime? dateDeleted;
+        public int Id { get; set; };
+        public ShippingOrderStatusEnum Status { get; set; };
+        public int SourceClientId { get; set; };
+        public int CreatorEmployeeId { get; set; };
+        public int DestinationAddressId { get; set; };
+        public int? FulfillerEmployeeId { get; set; };
+        public DateTime? ShippingDate { get; set; };
+        public DateTime DateCreated { get; set; };
+        public DateTime? DateModified { get; set; };
+        public DateTime? DateDeleted { get; set; };
         // private Client sourceClient;
         // TODO: @CINDY Les propriétés de navigation doivent être publiques et marquées 'virtual' pour permettre le lazy loading.
-        private Shipment? shipment;
-        private User creatorEmployee;
-        private User? fulfillerEmployee;
-        private Adresse destinationAddress;
-        // private List<ShippingOrderProduct> ShippingOrderProducts;
+        public Shipment? sSipment { get; set; };
+        public User CreatorEmployee { get; set; };
+        public User? FulfillerEmployee { get; set; };
+        public Adresse DestinationAddress { get; set; };
+        public virtual List<ShippingOrderProduct> ShippingOrderProducts { get; set; }=new List<ShippingOrderProduct>();
     }
 }
