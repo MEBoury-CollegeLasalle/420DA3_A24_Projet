89f5c9f6b3be17100febd59e00eacb777e852fa9
Marc-Eric Boury <marc-eric.boury@collegelasalle.com>
2024-11-08 15:17:06 -0500

    décommenté prop de navigation dans PurchaseOrder et ajout 'virtual'

--- 420DA3_A24_Projet/Business/Domain/PurchaseOrder.cs
+++ 420DA3_A24_Projet/Business/Domain/PurchaseOrder.cs
@@ -1,9 +1,4 @@
 ﻿using Project_Utilities.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _420DA3_A24_Projet.Business.Domain;
 
@@ -17,14 +12,14 @@ public class PurchaseOrder {
 
     public int Id { get; set; }
     public PurchaseOrderStatusEnum Status { get; set; }
-    public int Quantity { 
-        get { return this.quantity; } 
-        set { 
+    public int Quantity {
+        get { return this.quantity; }
+        set {
             if (!this.ValidateQuantity(value)) {
                 throw new ArgumentOutOfRangeException("Quantity", "Quantity must be greater than or equal to 1.");
             }
             this.quantity = value;
-        } 
+        }
     }
     public int OrderedProductId { get; set; }
     public int DestinationWarehouseId { get; set; }
@@ -35,8 +30,8 @@ public class PurchaseOrder {
     public byte[] RowVersion { get; set; } = null!;
 
 
-    //public Product OrderedProduct { get; set; } = null!;
-    //public Warehouse DestinationWarehouse { get; set; } = null!;
+    public virtual Product OrderedProduct { get; set; } = null!;
+    public virtual Warehouse DestinationWarehouse { get; set; } = null!;
 
 
     /// <summary>
