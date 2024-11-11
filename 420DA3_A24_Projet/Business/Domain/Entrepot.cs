1d4cfbaa11ac4bd8c03d47155223a4024ba3f42f
Gracmandiangu <grace.mandiangu_ndonzuau@lcieducation.net>
2024-11-11 10:28:51 -0500

    Creation de la classe Entrepot

---
+++ 420DA3_A24_Projet/Business/Domain/Entrepot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420DA3_A24_Projet.Business.Domain;
+internal class Entrepot {
+    public int Id { get; set; }
+    public string nomEntrepot { get; set; }
+    public string adresse { get; set; }
+    public DateTime? dateCreated { get; set; }
+    public DateTime? dateModified { get; set; }
+    public DateTime? dateDeleted { get; set; }
+
+}
--- 420DA3_A24_Projet/Business/Domain/Produit.cs
+++ 420DA3_A24_Projet/Business/Domain/Produit.cs
@@ -9,20 +9,18 @@ namespace _420DA3_A24_Projet.Business.Domain;
 public class Produit {
 
     public int Id { get; set; }
-    public String nom_produit { get; set; }
-    private String description_produit;
-    private String Code_UPC_International;
-    private Client proprietaireProduit;
-    private fournisseur fournisseur;
-    private int QtéStock;
-    private int QtéStockVisé;
-    private float poids;
-    private DateTime dateModified;
-    private DateTime dateDelete;
-    private DateTime dateCReated;
+    public string nomProduit { get; set; }
+    public string descriptionProduit { get; set; }
+    public string CodeUpcInternational { get; set; }
+    public Client proprietaireProduit { get; set; }
+    public fournisseur fournisseur { get; set; }
+    public int QteStock { get; set; }
+    public int QteStockVise { get; set; }
+    public float poids { get; set; }
+    public DateTime? dateModified { get; set; }
+    public DateTime? dateDelete { get; set; }
+    public DateTime? dateCReated { get; set; }
     public virtual List<ShippingOrderProduct> OrgderProducts { get; set; }
 
-        //Constructeur
-
-        public produit() { }
+        
 }
