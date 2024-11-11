a286a2fd89a4de03343eef5b4992438a460a75d5
saber <sellamisaber6@gmail.com>
2024-11-10 22:23:04 -0500

    Ajout des classes Expedition et Adresse

---
+++ 420DA3_A24_Projet/Business/Domain/Adresse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420DA3_A24_Projet.Business.Domain;
+public class Adresse {
+    public int AdresseId { get; set; }
+    public string TypeAdresse { get; set; }
+    public string Destinataire { get; set; }
+    public string NumeroCivique { get; set; }
+    public string Rue { get; set; }
+    public string Ville { get; set; }
+    public string Province { get; set; }
+    public string Pays { get; set; }
+    public string CodePostal { get; set; }
+    public DateTime DateCreation { get; set; } = DateTime.Now;
+    public DateTime? DateModification { get; set; }
+    public DateTime? DateSuppression { get; set; }
+
+    public void MettreAJourAdresse(string rue, string ville, string province, string pays, string codePostal) {
+        Rue = rue;
+        Ville = ville;
+        Province = province;
+        Pays = pays;
+        CodePostal = codePostal;
+        DateModification = DateTime.Now;
+    }
+}
---
+++ 420DA3_A24_Projet/Business/Domain/Expedition.cs
@@ -0,0 +1,20 @@
+﻿using _420DA3_A24_Projet.Business.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420DA3_A24_Projet.Business.Domain;
+public class Expedition {
+    public int ExpeditionId { get; set; }
+    public string ServiceLivraison { get; set; }
+    public string CodeSuivi { get; set; }
+    public int OrdreExpeditionId { get; set; }
+    public virtual ShippingOrder OrdreExpedition { get; set; } = null!;
+    public DateTime DateCreation { get; set; } = DateTime.Now;
+    public DateTime? DateModification { get; set; }
+    public DateTime? DateSuppression { get; set; }
+
+    
+}
