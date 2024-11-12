acb1dea80e2eb367a9234c0157114f9886075576
Khallouli608 <medkhalil.khallouli2002@gmail.com>
2024-11-12 01:27:41 -0500

    Modifier les calsses clients et fournisseur

--- 420DA3_A24_Projet/Business/Domain/Client.cs
+++ 420DA3_A24_Projet/Business/Domain/Client.cs
@@ -5,5 +5,36 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace _420DA3_A24_Projet.Business.Domain;
-internal class Client {
+public class Client {
+    public int Id { get; set; }
+    public string NomCompagnie { get; set; }
+    public string NomContact { get; set; }
+    public string PrenomContact { get; set; }
+    public string CourrielContact { get; set; }
+    public string TelephoneContact { get; set; }
+    public DateTime DateCreation { get; set; }
+    public DateTime? DateModification { get; set; }
+    public DateTime? DateSuppression { get; set; }
+    public int EntrepotId { get; set; }
+
+    public virtual Entrepot Entrepot { get; set; } = null!;
+    public virtual ICollection<Produit> Produits { get; set; } = new List<Produit>();
+
+   
+    public Client() { }
+
+   
+    public Client(string nomCompagnie, string nomContact, string prenomContact, string courrielContact, string telephoneContact, int entrepotId) {
+        NomCompagnie = nomCompagnie;
+        NomContact = nomContact;
+        PrenomContact = prenomContact;
+        CourrielContact = courrielContact;
+        TelephoneContact = telephoneContact;
+        EntrepotId = entrepotId;
+        DateCreation = DateTime.Now;
+    }
+
+    public override string ToString() {
+        return $"{NomCompagnie} (Contact: {PrenomContact} {NomContact}, Email: {CourrielContact}, Téléphone: {TelephoneContact})";
+    }
 }
