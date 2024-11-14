f0cbebbb6266922aba3b76aca1a37b00b2633cd3
Gracmandiangu <grace.mandiangu_ndonzuau@lcieducation.net>
2024-11-14 15:33:05 -0500

    modification de la classe entrepot

--- 420DA3_A24_Projet/Business/Domain/Entrepot.cs
+++ 420DA3_A24_Projet/Business/Domain/Entrepot.cs
@@ -6,11 +6,86 @@ using System.Threading.Tasks;
 
 namespace _420DA3_A24_Projet.Business.Domain;
 internal class Entrepot {
-    public int Id { get; set; }
+
+    //Identifiant
+    public int id { get; set; }
+
+    //Données entrepot
     public string nomEntrepot { get; set; }
     public string adresse { get; set; }
+
+    //Meta données
     public DateTime? dateCreated { get; set; }
     public DateTime? dateModified { get; set; }
     public DateTime? dateDeleted { get; set; }
+    public byte[] rowVersion { get; set; } = null!;
+
+    //Proprietés de navigation EF Core
+
+
+    ///<summary>
+    ///L'utilisateur est associé a cet entrepot
+    ///</summary>
+    public virtual User User { get; set; } = null!;
+
+    ///<summary>
+    ///Le produit est associé a cet entrepot
+    /// </summary>
+
+    public virtual Produit Produit { get; set; } = null!;
+
+
+    ///<summary>
+    ///constructeur orienté vers créeation manuelle. 
+    ///</summary>
+    ///<param name="nomEntrepot">Le nom de l'entrepot</param>
+    ///<param name="UserId">L'identifiant de de l'utilisateur</param>
+    ///<param name="ProduitId">L'identifiant du produit</param>
+    
+    public Entrepot(int Id , string NomEntrepot, string Adresse ) {
+        this.id = Id;
+        this.nomEntrepot = NomEntrepot;
+        this.adresse = Adresse;
+    }
+
+    /// <summary>
+    /// constructeur orienté création par Entity Framework.
+    /// </summary>
+    /// <param name="id">L'identifiant de l'entrepot</param>
+    /// <param name="nomEntrepot">Le nom de l'entrepot</param>
+    /// <param name="adresse">L'adresse de l'entrepot</param>
+    /// <param name="dateCreated">La date de la creation de l'entrepot</param>
+    /// <param name="dateModified">La date de la derniere modification de l'entrepot</param>
+    /// <param name="dateDeleted">La date de la suppression de l'entrepot</param>
+    /// /// <param name="rowVersion">Valeur anti-concurrence ge la base de données.</param>
+
+
+    protected Entrepot(
+        int Id,
+        string NomEntrepot,
+        string Adresse,
+        DateTime dateCreated,
+        DateTime? dateModified,
+        DateTime? dateDeleted,
+        byte[] rowVersion)
+        : this(Id , NomEntrepot, Adresse) {
+
+        this.id = Id;
+        this.dateCreated = dateCreated;
+        this.dateModified = dateModified;
+        this.dateDeleted = dateDeleted;
+        this.rowVersion = rowVersion;
+    }
+
+    ///<summary>
+    /// override de la methode ToString pour afficher les information de l'entrepot
+    /// </summary>
+    /// <return> un string representant un entrepot</return>
+
+    public override string ToString() {
+        return $"#{this.id} - {this.nomEntrepot} - {this.adresse}";
+    }
+
+
 
 }
--- 420DA3_A24_Projet/Business/Domain/Produit.cs
+++ 420DA3_A24_Projet/Business/Domain/Produit.cs
@@ -7,20 +7,42 @@ using System.Threading.Tasks;
 
 namespace _420DA3_A24_Projet.Business.Domain;
 public class Produit {
-
+    //Identifiant
     public int Id { get; set; }
-    public string nomProduit { get; set; }
-    public string descriptionProduit { get; set; }
-    public string CodeUpcInternational { get; set; }
+
+    //Données du Produit
+    public string nomproduit { get; set; }
+    public string descriptionproduit { get; set; }
+    public string codeUpcinternational { get; set; }
     public Client proprietaireProduit { get; set; }
     public fournisseur fournisseur { get; set; }
     public int QteStock { get; set; }
     public int QteStockVise { get; set; }
     public float poids { get; set; }
+
+    //Meta données 
     public DateTime? dateModified { get; set; }
     public DateTime? dateDelete { get; set; }
     public DateTime? dateCReated { get; set; }
     public virtual List<ShippingOrderProduct> OrgderProducts { get; set; }
+
+
+    //Proprietés de navigation EF Core
+
+
+    ///<summary>
+    ///L'utilisateur est associé a cet entrepot
+    ///</summary>
+    public virtual User User { get; set; } = null!;
+
+    ///<summary>
+    ///Le produit est associé a cet entrepot
+    /// </summary>
+    
+    public virtual 
+
+
+
 
         
 }
